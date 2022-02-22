bf3353e150edf3593e078a45be13f5f7c8499763
Eduardo Pinto <eduardo.pinto@shopify.com>
2022-02-22 13:47:12 +0000

    Add expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const expenses = [
   {
@@ -30,8 +31,9 @@ const expenses = [
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
+      
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [updateCount, setUpdateCount] = useState(0);
+  const clickHandler = () => setUpdateCount(updateCount + 1);
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} locale={props.locale} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>
+          {updateCount > 0
+            ? `${props.title} update ${updateCount} times`
+            : props.title}
+        </h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
